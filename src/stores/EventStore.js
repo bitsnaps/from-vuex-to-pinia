c2d183af5c90a7cfd21a5fc954427fe4b7d91c5d
Ibrahim.H <bitsnaps@yahoo.fr>
2022-07-22 19:38:07 +0100

    step5: changing a state with actions

--- src/stores/EventStore.js
+++ src/stores/EventStore.js
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia'
+import EventService from '../services/EventService.js'
 
 export const useEventStore = defineStore('EventStore', {
     state() {
@@ -6,5 +7,22 @@ export const useEventStore = defineStore('EventStore', {
             events: [],
             event: {}
         }
-    }    
+    },
+    getters: {
+        numberOfEvents: state => state.events.length
+
+    },
+    actions: {
+        // We don't need commit() function here as we do in vuex
+        fetchEvents() {
+            return EventService.getEvents()
+              .then(response => {
+                this.events = response.data
+              })
+              .catch(error => {
+                throw error
+              })
+          }        
+    }
+    
 })
